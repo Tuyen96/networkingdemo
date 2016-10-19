e74a29f3041277e354579892a54b269d56131666
Tuyen <tuyen30101996@gmail.com>
2016-10-19 20:40:57 +0700

    1

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+post1 = {
+    "title" : "Good day",
+    "content": "I met a girl."
+}
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello Tuyen '
+    return json.dumps(post1)
 
 
 if __name__ == '__main__':
